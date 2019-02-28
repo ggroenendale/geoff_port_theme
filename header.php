33e95c486dc8d2e1bd4daae3bc418f2a8daa3ce6
ggroenendale <ggroenendale@gmail.com>
2019-02-27 16:45:50 -0800

    Latest Modifications

--- header.php
+++ header.php
@@ -11,7 +11,16 @@
 ?>
 <header>
 	<div id="logo">
-		<img src="https://via.placeholder.com/60x60?text=LogoPH">
+
+		<?php
+			$logo_src = wp_get_attachment_image_url(get_theme_mod('port_logo'), 'full');
+			if (!empty($logo_src)) {
+				echo '<a class="logo-a" href="' . esc_url(home_url('/')) . '"><img id="logo" src="' . $logo_src . '"></a>';
+			}
+			else {
+				echo '<a class="logo-a" href="' . esc_url(home_url('/')) . '">' . $logo_src . '<img src="https://via.placeholder.com/100x100?text=LogoPH"></a>';
+			}
+		?>
 	</div>
 	<nav>
         <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
